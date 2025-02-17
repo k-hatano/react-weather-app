0cc6a5b0f7be28466c46ac0472df7aaec7cbde64
k-hatano <kent.ruffle.mgj626@gmail.com>
2025-02-18 00:58:44 +0900

    Support converting temperature (Celsius <-> Fahrenheit)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+function showCelsiusTemperature() {
+	document.getElementsByClassName("temperature_celsius")[0].className = "temperature_celsius";
+	document.getElementsByClassName("temperature_fahrenheit")[0].className = "temperature_fahrenheit hidden";
+}
+
+function showFahrenheitTemperature() {
+	document.getElementsByClassName("temperature_celsius")[0].className = "temperature_celsius hidden";
+	document.getElementsByClassName("temperature_fahrenheit")[0].className = "temperature_fahrenheit";
+}
+
 const Weather = props => (
 	<div className="weather__info">
 	 {	
@@ -9,7 +19,15 @@ const Weather = props => (
 	 }
 	 { 	
 	 	props.temperature && <p className="weather__key"> Temperature: 
-	 		<span className="weather__value"> { props.temperature }	</span><span className="weather__unit">℃</span>
+ 			<span className="temperature_celsius">
+ 				<span className="weather__value"> <a href="javascript:void(0);" onClick={showFahrenheitTemperature}> { props.temperature }</a> </span>
+ 				<span className="weather__unit temperature_celsius">℃</span>
+	 			
+	 		</span>
+	 		<span className="temperature_fahrenheit hidden">
+ 				<span className="weather__value"> <a href="javascript:void(0);" onClick={showCelsiusTemperature}>{ ( props.temperature ) * (9 / 5) + 32 }</a> </span>
+ 				<span className="weather__unit">°F</span>
+	 		</span>
 	 	</p> 
 	 }
 	 { 	
@@ -25,7 +43,7 @@ const Weather = props => (
 	 { 	
 	 	props.description && <p className="weather__key"> Conditions: 
 	 		<span className="weather__value"> <a href={"https://openweathermap.org/find?q=" + props.city + "," + props.country} target="_blank">{ props.description }</a> </span>
-	 </p> 
+	 	</p> 
 	 }
 	 { 
 	 	props.error && <p className="weather__error">{ props.error }</p>  
